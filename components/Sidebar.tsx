08144afeca19a3aa4f3588f94bf4f1f65af36788
Aditya Singh <adityasinghtech011@gmail.com>
2024-11-24 22:28:14 +0530

    updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,7 @@
+// components/Sidebar.tsx
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSidebar } from '@/contexts/SidebarContext';
 import { useRouter } from 'next/navigation';
 import { Lists, useLists } from '@/contexts/ListContext';
@@ -9,9 +10,6 @@ const Sidebar = () => {
   const { isCollapsed, toggleSidebar } = useSidebar();
   const { lists, selectedList, selectList, fetchLists } = useLists();
   const router = useRouter();
-
-  // Track screen size for collapsing the sidebar
-  const [isMobile, setIsMobile] = useState(false);
 
   const handleNavigation = (list: Lists) => {
     if (!list) return;
@@ -23,36 +21,14 @@ const Sidebar = () => {
     router.push(`/dashboard/?id=${list.id}`);
     selectList(list);
   };
-
   useEffect(() => {
     fetchLists();
-
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
   }, [lists]);
-
-  useEffect(() => {
-    if (isMobile) {
-      toggleSidebar();
-    } else {
-      toggleSidebar();
-    }
-  }, [isMobile, toggleSidebar]);
-
   return (
     <div
       className={`transition-transform transform ${
         isCollapsed ? '-translate-x-full' : 'translate-x-0'
-      } fixed z-10 w-64 bg-[#F1F2F7] h-full border md:translate-x-0`}
+      } fixed  z-10 w-64 bg-[#F1F2F7] h-full border `}
     >
       <div className='flex justify-between items-center h-14 px-4 border-b border-[#C8CBD9]'>
         <h2 className='font-bold text-lg'>Notes</h2>
@@ -63,7 +39,10 @@ const Sidebar = () => {
       <div className='py-2 mt-4 h-full justify-between flex flex-col gap-2'>
         <div className='flex flex-col gap-6'>
           <button
-            onClick={() => router.push('/dashboard')}
+            onClick={() => {
+              toggleSidebar();
+              router.push('/dashboard');
+            }}
             className='text-green-500 border mx-4 rounded bg-slate-50 border-green-500 px-2 py-1 hover:bg-green-500 hover:text-white'
           >
             + Create
